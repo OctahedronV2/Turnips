0b7cc518dd5eccafa3d0c8527aef24435ebf758e
Octahedron <40345761+OctahedronV2@users.noreply.github.com>
2021-06-11 02:13:02 -0400

    Added basic custom settings
    Just a few settings so that I could figure out how they work.

--- Turnips_StreamlabsSystem.py
+++ Turnips_StreamlabsSystem.py
@@ -1,18 +1,61 @@
+#---------------------------
+#   Import Libraries
+#---------------------------
+
 import json
 import random
 import math
 import time
 import os
+import sys
+import codecs
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib")) #point at lib folder for classes / references
 
+import clr
+clr.AddReference("IronPython.SQLite.dll")
+clr.AddReference("IronPython.Modules.dll")
+
+#---------------------------
+#   [Required] Script Information
+#---------------------------
 ScriptName = "Turnips"
-Description = "Adds the stock market to your bot"
-Creator = "Octahedron#5752"
+Description = "Adds the stock market to your bot."
+Creator = "Octahedron"
 Version = "1.0.1"
 SpecialThanks = "https://www.twitch.tv/hypherius"
 Website = ""
 
+settingsfile = os.path.join(os.path.dirname(__file__), "settings.json")
+
 JsonFile = os.path.join(os.path.dirname(__file__), "turnips.json")
 PriceTimer = 15 * 60 * 1000 # 15 minutes
+
+class Settings(object):
+    """ Load in saved settings file if available else set default values. """
+    def __init__(self, settingsfile=None):
+        Parent.Log(ScriptName, "Creating Settings Object")
+        if settingsfile and os.path.isfile(settingsfile):
+            with codecs.open(settingsfile, encoding="utf-8-sig", mode="r") as f:
+                self.__dict__ = json.load(f, encoding="utf-8")
+        else:
+            Parent.Log(ScriptName, "Setting To Default")
+            self.Command = "!turnips"
+            self.Cooldown = 4
+            self.HelpMessage = "Check out the Turnips readme: https://github.com/OctahedronV2/Turnips/blob/main/README.md!"
+
+	def Reload(self, jsondata):
+		""" Reload settings from Streamlabs user interface by given json data. """
+		self.__dict__ = json.loads(jsondata, encoding="utf-8")
+
+	def Save(self, settingsfile):
+		""" Save settings contained within to .json and .js settings files. """
+		try:
+			with codecs.open(settingsfile, encoding="utf-8-sig", mode="w+") as f:
+				json.dump(self.__dict__, f, encoding="utf-8", ensure_ascii=False)
+			with codecs.open(settingsfile.replace("json", "js"), encoding="utf-8-sig", mode="w+") as f:
+				f.write("var settings = {0};".format(json.dumps(self.__dict__, encoding='utf-8', ensure_ascii=False)))
+		except:
+			Parent.Log(ScriptName, "Failed to save settings to file.")
 
 def readJson():
     with open(JsonFile) as json_file:
@@ -30,7 +73,17 @@ def verifyTurnipBalance(user):
         writeJson(jsonData)
 
 def Init():
-    pass
+
+    global ScriptSettings
+    ScriptSettings = Settings(settingsfile)
+
+    return
+
+def ReloadSettings(jsondata):
+
+    Init()
+
+    return
 
 def Execute(data):
     if not data.IsChatMessage():
@@ -38,7 +91,12 @@ def Execute(data):
 
     command = data.GetParam(0).lower()
 
-    if (command == "!turnips") or (command == "!turnip"):
+    if command == ScriptSettings.Command:
+        if Parent.IsOnUserCooldown(ScriptName, ScriptSettings.Command, data.User):
+            cooldown = Parent.GetUserCooldownDuration(ScriptName, ScriptSettings.Command, data.User)
+            Parent.SendStreamMessage("You are on cooldown for this command for another: " + str(cooldown) + " second"  + ("s." if cooldown != 1 else "."))
+            return
+        Parent.AddUserCooldown(ScriptName, ScriptSettings.Command, data.User, ScriptSettings.Cooldown)
         jsonData = readJson()
         verifyTurnipBalance(data.User)
         turnipBalance = jsonData['userBalances'][data.User]
@@ -52,10 +110,10 @@ def Execute(data):
         secondsUntilUpdate = secondsUntilUpdate - minutesUntilUpdate * 60
 
         if (data.GetParam(1) == "") or (data.GetParam(1).lower() == "help"):
-            Parent.SendStreamMessage("Check out the Turnips readme: https://github.com/OctahedronV2/Turnips/blob/main/README.md!")
+            Parent.SendStreamMessage(ScriptSettings.HelpMessage)
 
         elif (data.GetParam(1).lower() == "balance"):
-            Parent.SendStreamMessage("You have " + str(turnipBalance) + " turnips!");
+            Parent.SendStreamMessage("You have " + str(turnipBalance) + " turnip" + ("s." if cooldown != 1 else "."));
 
         elif (data.GetParam(1).lower() == "price") or (data.GetParam(1).lower() == "value"):
             Parent.SendStreamMessage("Turnips are currently worth " + str(turnipPrice) + " Trashbucks! The price will update in: " + str(minutesUntilUpdate) + "m " + str(secondsUntilUpdate) + "s ");
@@ -78,7 +136,7 @@ def Execute(data):
                 jsonData["userBalances"][data.User] += quantity
                 writeJson(jsonData)
                 Parent.RemovePoints(data.User, data.UserName, price)
-                Parent.SendStreamMessage("Successfully purchased " + str(quantity) + " turnips for " + str(price) + " Trashbucks!")
+                Parent.SendStreamMessage("Successfully purchased " + str(quantity) + (" turnips for " if quantity != 1 else " turnip for ") + str(price) + " Trashbucks!")
             else:
                 Parent.SendStreamMessage("You cannot afford that many turnips.")
 
@@ -101,6 +159,7 @@ def Execute(data):
                 writeJson(jsonData)
                 Parent.AddPoints(data.User, data.UserName, value)
                 Parent.SendStreamMessage("Successfully sold " + str(quantity) + " turnips for " + str(value) + "!")
+        return
 
 def Tick():
     currentTime = int(round(time.time() * 1000))
---
+++ settings.js
@@ -0,0 +1,5 @@
+﻿var settings = {
+  "Command": "!turnips",
+  "Cooldown": 5.0,
+  "HelpMessage": "Check out the Turnips readme for help: https://github.com/OctahedronV2/Turnips/blob/main/README.md!"
+};
