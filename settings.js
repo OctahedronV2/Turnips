c2f03a6c5d06dcf6fdebed1300c406e7381f45a2
Octahedron <40345761+OctahedronV2@users.noreply.github.com>
2021-06-11 20:09:25 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,1 @@
-﻿var settings = {
-  "Command": "!turnips",
-  "Cooldown": 5.0,
-  "HelpMessage": "Check out the Turnips readme for help: https://github.com/OctahedronV2/Turnips/blob/main/README.md!"
-};
+﻿var settings = {};
